53596cffa804e56aa3d6b71bf9a99516e5d91df6
Eric Chang <eric.chang@opower.com>
2014-06-25 11:27:59 -0700

    initial state from chef server

---
+++ .chef/knife.rb
@@ -0,0 +1,14 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "ericchang"
+#client_key               "#{current_dir}/ericchang1.pem"
+client_key               "#{current_dir}/ericchang.pem"
+validation_client_name   "eric-inc-validator"
+validation_key           "#{current_dir}/eric-inc-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/eric-inc"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
